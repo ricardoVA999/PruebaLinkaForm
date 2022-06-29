9bc3117fbc253488321947b210948a82a1e29c21
Luis-Alfredo-Felipe <106787972+Luis-Alfredo-Felipe@users.noreply.github.com>
2022-06-29 12:25:19 -0600

    Add files via upload

---
+++ DireccionIP_Data.js
@@ -0,0 +1,30 @@
+
+var columnsData = [
+  {"title": "Direccion IP", "frozen": false, "hozAlign": "left", "field": "Direccion IP", "with": 150},
+  {"title": "Mac Address", "frozen": false, "hozAlign": "left", "field": "Mac Address", "with": 150},
+  {"headerFilter": "input", "title": "Descripción",  "field": "Descripción", "frozen": false, "hozAlign": "center", "with": 150},
+  {"title": "Nombre de Area",  "field": "Nombre de Area", "frozen": false, "hozAlign": "center", "with": 150},
+  {"title": "Colaborador",  "field": "Colaborador", "frozen": false, "hozAlign": "center", "with": 150}
+]
+
+
+/*var dataTable1 = [
+ {id:1, plant_name:"Oli Bob", gender:"female", actuals202238:"12", col:"red", dob:""},
+ {id:2, plant_name:"Mary May", actuals202238:"1", col:"blue", dob:"14/05/1982"},
+ {id:3, plant_name:"Christine Lobowski", actuals202238:"42", col:"green", dob:"22/05/1982"},
+ {id:4, plant_name:"Brendon Philips", actuals202238:"125", col:"orange", dob:"01/08/1980"},
+ {id:5, plant_name:"Margret Marmajuke", actuals202238:"16", col:"yellow", dob:"31/01/1999"},
+];
+*/
+
+var dataTable1 = [
+  {
+    "Direccion IP ":"192.168.1.1", "Mc Address":"12-34-56-78-9A-BC", "Descripcion":"Direccion Unica PC",
+    "Nombre de Area":"Departamento IT", "Colaborador":"Rafael Marquez", 
+  }
+
+];
+
+
+
+
---
+++ DireccionamientoIP.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#####
+#####
+# Script para indicar quien asigno el regsitro
+#
+#
+#####
+import sys, json, simplejson#, subprocess
+#from bson import ObjectId
+
+from datetime import datetime, timedelta
+import time
+
+from linkaform_api import settings
+from linkaform_api import network
+from settings_selecom import *
+
+
+
+def query_data(form_id, Area, Usuario):
+    global report_model
+    match_query = {
+        "form_id":form_id,
+        "deleted_at":{"$exists":False}
+    }
+
+    if Area:
+        match_query.update({'answers.60007c5194d9a63b1caf534c.60007ca59253268e682b8760':Area})
+    if Usuario:
+        match_query.update({'answers.60007c5194d9a63b1caf534c.60007c5194d9a63b1caf534f':Usuarios})
+
+    else:
+        project = {
+            "_id":0,
+            "Direccion IP":"$answers.60c28676eb621713f3908761",
+            "Mac Address":"$answers.60c28676eb621713f3908762",
+            "Descripción":"$answers.60c28676eb621713f3908763",
+            "Nombre de Area":"$answers.60007c5194d9a63b1caf534c.60007ca59253268e682b8760",
+            "Colaborador":"$answers.60007c5194d9a63b1caf534c.60007c5194d9a63b1caf534f"
+        }
+
+    query = [
+        {"$match": match_query },
+
+        {"$project":project},
+    ]
+
+    solicitudes = cr.aggregate(query)
+
+    res = []
+
+    for s in solicitudes:
+        res.append(s)
+
+    return res
+
+if __name__ == '__main__':
+    print(sys.argv)
+    jwt_complete = simplejson.loads(sys.argv[2])
+    all_data = simplejson.loads(sys.argv[2])
+    data = all_data.get("data", {})
+    config['USER_JWT_KEY'] = jwt_complete["jwt"].split(' ')[1]
+    settings.config.update(config)
+    form_id = data.get("form_id",71726)
+    Area = data.get("Area","")
+    Usuario = data.get("Usuario","")
+
+    if '--' in Area:
+        Area = None
+    if '--' in Usuario:
+        Usuario = None
+
+    net = network.Network(settings)
+    cr = net.get_collections()
+    lkf_api = utils.Cache(settings)
+    table_data = query_data(form_id, Area, Usuario)
+    sys.stdout.write(simplejson.dumps({
+        "firstElement":{'tabledata':table_data},
+        }))
